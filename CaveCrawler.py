be0297c96fe063d91c3a7df8e62bf34ded56725d
nbovee <12849851+nbovee@users.noreply.github.com>
2022-05-07 19:07:21 -0400

    Update CaveCrawler.py

--- CaveCrawler.py
+++ CaveCrawler.py
@@ -1,23 +1,23 @@
+
+from typing import Tuple, Iterator
 from spike import PrimeHub, DistanceSensor, Motor, MotorPair
-from math import radians, cos, sin, pi
+from math import radians, cos, sin, pi, copysign
 from utime import sleep as wait_for_seconds
 
-import sys
+# some mapping code inspired from https://www.redblobgames.com/pathfinding/a-star/implementation.html
 import gc
-import array
+import uarray as array
 # from micropython import const
 
 # things that would usually be in a config file
 wheel_base_cm = 12.2
 wheel_diameter_cm = 5.6
-# update this
+# is this right offset?
 l_ultrasonic_offset = r_ultrasonic_offset = 6
-# f_ultrasonic_offset = 8.5
-# l_motor_backlash_deg = 5
-# r_motor_backlash_deg = 6
+l_us_angle = - 90
+r_us_angle = 90
 l_motor = "E"
 r_motor = "F"
-# f_ultrasonic = "A"
 l_ultrasonic = "A"
 r_ultrasonic = "C"
 turret_motor = "D"
@@ -27,27 +27,79 @@ CONST_MAX_SPIKE_CM_DIST = 200
 CONST_DETECTION_MAX_DIST_CM = 30
 CONST_APPROACH_DIST_CM = 4
 CONST_WAIT_TIME = 0.1
-CONST_MAP_CELL_SIZE = 2
+CONST_MAP_EDGE = 40
+CONST_MAP_CELL_SIZE = 10
+CONST_MOTOR_SPEED = 30
+CONST_TURRET_SPEED = 20
+CONST_STOP_ACTION = "hold"
+CONST_TURRET_HOME = 0
+CONST_TURRET_ANGLE_STEP = 6
+CONST_TURRET_SWEEP_END = 180 + CONST_TURRET_HOME + CONST_TURRET_ANGLE_STEP + 1
+
+Map_Location = Tuple[int, int]
+
+class Map:
+    def __init__(self, width: int, height: int) -> None:
+        self.map = []
+        self.width = width
+        self.height = height
+        for i in range(height):
+            self.map.append(array.array(map_storage_vals["d_type"]))
+            for j in range(width):
+                self.map[i].append(map_storage_vals["new"])
+
+    def add_to_map(self, id: Map_Location, value):
+        (x, y) = id
+        if self.in_bounds(id):
+            self.map[x][y] = value
+
+    def in_bounds(self, id: Map_Location) -> bool:
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+    
+    def neighbors(self, id: Map_Location) -> Iterator[Map_Location]:
+        (x, y) = id
+        neighbors = [(x+1, y), (x-1, y), (x, y-1), (x, y+1)] # E W N S
+        if (x + y) % 2 == 0: neighbors.reverse() # S N W E
+        results = filter(self.in_bounds, neighbors)
+        # results = filter(self.passable, results)
+        return results
+
+    def cell(self, id: Map_Location):
+        (x, y) = id
+        if filter(self.in_bounds, id ):
+            return map[x][y]
+        else:
+            raise Exception("Out of Bounds")
+
 
 # initialize global objects
 hub = PrimeHub()
 motors = MotorPair(l_motor, r_motor)
 turret = Motor(turret_motor)
-motors.set_stop_action("hold")
-turret.set_stop_action("hold")
-turret.set_default_speed(20)
-# f_ultrasonic = DistanceSensor(f_ultrasonic)
+motors.set_stop_action(CONST_STOP_ACTION)
+turret.set_stop_action(CONST_STOP_ACTION)
+turret.set_default_speed(CONST_TURRET_SPEED)
 l_ultrasonic = DistanceSensor(l_ultrasonic)
 r_ultrasonic = DistanceSensor(r_ultrasonic)
 
                     #type, unknown, empty, wall, robot
-# map_storage_vals = ["B", bytes("-1", "ascii"), "0", "1", "2"]
-map_storage_vals = ["h", 0, -1, 1, 2]
+# map_storage_vals = ["b", '?', '-', '#', 'R']
+# map_storage_vals = ["b", b'x/00', b'x\01', b'x\02', b'x\03']
+# map_storage_vals = ["h", 0, -1, 1, 2] 
+
+map_storage_vals = {
+    "d_type" : 'h',
+    "new" : 0,
+    "empty" : -1,
+    "obstacle" : 1,
+    "robot" : 2
+}
 
 def drive_for_cm(cm):
     '''move the robot the specific number of cm forward'''
     rotation = 360 * cm / (pi * wheel_diameter_cm)
-    motors.move_tank(rotation, unit="degrees", left_speed = 30, right_speed = 30)
+    motors.move_tank(rotation, unit="degrees", left_speed = CONST_MOTOR_SPEED, right_speed = CONST_MOTOR_SPEED)
 
 def turn_for_degrees(degrees):
     '''rotate the robot the specified number of degrees'''
@@ -56,47 +108,51 @@ def turn_for_degrees(degrees):
     cm = base_circumference * degrees / 360
     rotation = 360 * cm / (pi * wheel_diameter_cm)
     # I do not know why it makes me do it this way
-    motors.move_tank(sign * rotation, unit="degrees", left_speed = sign * 30, right_speed = -sign *30)
+    motors.move_tank(sign * rotation, unit="degrees", left_speed = sign * CONST_MOTOR_SPEED, right_speed = -sign * CONST_MOTOR_SPEED)
 
 def take_turret_reading():
     '''return a pair of readings from the ultrasonic turret at the current location'''
     def take_distance_reading(ultrasonic):
         range = ultrasonic.get_distance_cm()
-        value = map_storage_vals[3]
+        value = map_storage_vals["obstacle"]
         if range is None:
             range = CONST_MAX_SPIKE_CM_DIST
-            value = map_storage_vals[2]
+            value = map_storage_vals["empty"]
         return (range, value)
-
     (range_l, value_l) = take_distance_reading(l_ultrasonic)
     wait_for_seconds(CONST_WAIT_TIME)
     (range_r, value_r) = take_distance_reading(r_ultrasonic)
-    wait_for_seconds(CONST_WAIT_TIME)
     heading = turret.get_position()
-    package = [(range_l + l_ultrasonic_offset, (heading - 90) % 360, value_l), (range_r + r_ultrasonic_offset, (heading + 90) % 360, value_r)]
-    # package = [(range_l + l_ultrasonic_offset, (heading - 90) % 360, value_l)]
-    
+    package = [(range_l + l_ultrasonic_offset, (heading + l_us_angle) % 360, value_l)]
+    package.extend([(range_r + r_ultrasonic_offset, (heading + r_us_angle) % 360, value_r)])
     return package
 
 def map_current_location(map, robot_location):
     '''take 360 degree ultrasonic readings from the current locations'''
     # list comprehension fails for some reason...
     # newmap = [[ 0 for g in range(2*200/CONST_MAP_CELL_SIZE)] for k in range(2*200/CONST_MAP_CELL_SIZE)]
-    turret.run_to_position(10, direction = "shortest path")
-    turret.run_to_position(359-5, direction = "counterclockwise")
+    # this should do some position checking to ensure no errors with tangled wires
+    turret.run_to_position(340, direction = "shortest path")
+    turret.run_to_position(354, direction = "clockwise")
     readings = []
-    for i in range(0,187,6):
+    for i in range(CONST_TURRET_HOME, CONST_TURRET_SWEEP_END, CONST_TURRET_ANGLE_STEP):
         wait_for_seconds(CONST_WAIT_TIME)
         readings.extend(take_turret_reading())
         turret.run_to_position(i, direction = "clockwise")
-    turret.run_to_position(359-5, direction = "counterclockwise")
+    turret.run_to_position(CONST_TURRET_HOME, direction = "counterclockwise")
     for reading in readings:
         (range_cm, heading, value) = reading
         # fill in detected cell
+        loc_val = reading_to_relative_coordinate(reading, robot_location)
+        add_coordinate_to_map(map, loc_val)
+        (x, y, _ ) = loc_val
+        # add neighbors as a buffer
+        for cell in map.neighbors((x, y)):
+            (t_x, t_y) = cell
+            add_coordinate_to_map(map, (t_x, t_y, value))
         # fill in empty cells between
-        for i in range(range_cm - 10, 0, -10):
-            add_coordinate_to_map(map, reading_to_relative_coordinate((i, heading, map_storage_vals[2]), robot_location))
-        add_coordinate_to_map(map, reading_to_relative_coordinate(reading, robot_location))
+        for i in range(range_cm - CONST_MAP_CELL_SIZE, 0, -CONST_MAP_CELL_SIZE):
+            add_coordinate_to_map(map, reading_to_relative_coordinate((i, heading, map_storage_vals["empty"]), robot_location))
 
 
 def reading_to_relative_coordinate(reading, robot_location):
@@ -104,17 +160,20 @@ def reading_to_relative_coordinate(reading, robot_location):
     # cos, sin
     # check that these are correctly offset
     (range, heading, value) = reading
-    range = range / 200 * 17
+    range = range / CONST_MAX_SPIKE_CM_DIST * CONST_MAP_CELL_SIZE
     x_coor = range*sin(radians(heading + robot_location[2]))
     # correct y_coordinate to N being 0 degrees for world coordinates
     y_coor = -range*cos(radians(heading + robot_location[2]))
     return (robot_location[0] + x_coor, robot_location[1] + y_coor, value)
 
-def add_coordinate_to_map(map, coordinate):
+def add_coordinate_to_map(map: Map, coordinate):
     '''edit a map coordinate to the specified value. Methodized to avoid mistakes.'''
     (x, y, value) = coordinate
-    # convert to map cell sizes smaller than 1cm.
-    map[int(x)][int(y)] = value
+    # only overwrite empty or new locations
+    current_val = map.cell((x, y))
+    #clunky
+    if current_val is not None and current_val == map_storage_vals["empty"] or current_val == map_storage_vals["new"]:
+        map.add_to_map((x,y), value)
 
 def offset_map(map, robot_location, offset):
     return None
@@ -123,23 +182,21 @@ def rotate_map(map, robot_location, angle):
     return None
 
 def printmap(map):
+    output = ''
     for i in map:
         for j in range(len(i)):
-            if i[j] == map_storage_vals[2]:
-                print(" ", end = "")
+            if i[j] == map_storage_vals["empty"]:
+                output += '  '
             else:
-                print(i[j], end = "")
-        print()
+                output += str(i[j]) + ' '
+        output += '\n'
+    print(output)
 
 def test_loop():
-    robot_location = [18, 18, 0]
-    newmap = []
-    for i in range(36):
-        newmap.append(array.array(map_storage_vals[0]))
-        for j in range(36):
-            newmap[i].append(map_storage_vals[1])
-    # map_current_location(newmap, robot_location)
-    newmap[18][18] = map_storage_vals[4]
+    robot_location = [int(CONST_MAP_EDGE/2), int(CONST_MAP_EDGE/2), 0]
+    newmap = Map(CONST_MAP_EDGE, CONST_MAP_EDGE)
+    map_current_location(newmap, robot_location)
+    newmap[18][18] = map_storage_vals["robot"]
     return newmap
 
 
@@ -164,13 +221,14 @@ def main():
     print(gc.mem_free())
     map = test_loop()
     print(gc.mem_free())
-    map2 = test_loop()
-    print(gc.mem_free())
-    map3 = test_loop()
-    print(gc.mem_free())
+    # map2 = test_loop()
+    # print(gc.mem_free())
+    # map3 = test_loop()
+    # print(gc.mem_free())
     printmap(map)
-    print()
-    printmap(map2)
+    # print()
+    # printmap(map2)
+    # map3 = 
 
     
     raise SystemExit
